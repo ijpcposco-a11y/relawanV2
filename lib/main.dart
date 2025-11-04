0a9c7800ae1225cffd62952b6d8f9768b38246e8
ijpcposco-a11y <ijpcposco@gmail.com>
2025-11-04 10:37:34 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'login_page.dart'; // halaman login
+import 'package:firebase_auth/firebase_auth.dart';
+import 'login_page.dart';
+import 'home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,8 +19,35 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Pendataan Relawan',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const LoginPage(),
       debugShowCheckedModeBanner: false,
+      home: const AuthGate(),
+    );
+  }
+}
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        // Firebase masih inisialisasi
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        }
+
+        // Jika ada user yang login
+        if (snapshot.hasData) {
+          return const HomePage();
+        }
+
+        // Jika belum login
+        return const LoginPage();
+      },
     );
   }
 }
